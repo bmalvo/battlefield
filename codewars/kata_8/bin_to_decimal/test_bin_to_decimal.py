c84c2ae3036c93609c326b054be47fd5d915c4d4
patryk_s <boydmalvo@gmail.com>
2023-05-26 21:29:15 +0200

    bin_to_decimal

---
+++ codewars/kata_8/bin_to_decimal/test_bin_to_decimal.py
@@ -0,0 +1,7 @@
+from bin_to_decimal import bin_to_decimal
+import pytest
+
+
+@pytest.mark.parametrize('given, expected', [(("0"), 0), (("10"), 2), (("11"), 3), (("101010"), 42), (("1001001"), 73)])
+def test_bin_to_decimal(given, expected):
+    assert bin_to_decimal(given) == expected
