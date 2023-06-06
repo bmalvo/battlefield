1e1b519e2f947c11df9cf9536ede3fd3d51fbe0d
patryk_s <boydmalvo@gmail.com>
2023-06-06 12:39:43 +0200

    count_pos_sum_nagative

---
+++ codewars/kata_8/count_pos_sum_negative/count_pos_sum_negative.py
@@ -0,0 +1,19 @@
+"""Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative. If the input is an empty array or is null, return an empty array. 
+
+Example: 
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]."""
+
+
+def count_positives_sum_negatives(arr):
+    """Like in description above"""
+    if not arr:
+        result = []
+    else:
+        result = []
+        result.append(sum(x > 0 for x in arr))
+        second_res = 0
+        for i in arr:
+            if i < 0:
+                second_res += i
+        result.append(second_res)
+    return result
