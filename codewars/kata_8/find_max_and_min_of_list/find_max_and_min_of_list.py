d6231e67b3a28a69e49e769f74e84d92c4c609f5
patryk_s <boydmalvo@gmail.com>
2023-06-26 13:19:20 +0200

    find_max_and_min_of_list

---
+++ codewars/kata_8/find_max_and_min_of_list/find_max_and_min_of_list.py
@@ -0,0 +1,11 @@
+"""Your task is to make two functions ( max and min, or maximum and minimum,\n etc., depending on the language ) that receive a list of integers as input, \nand return the largest and lowest number in that list, respectively."""
+
+
+def minimum(arr):
+    """Return min value from arr"""
+    return min(arr)
+
+
+def maximum(arr):
+    """Return max value from arr"""
+    return max(arr)
---
+++ codewars/kata_8/find_max_and_min_of_list/test_find_max_and_min_of_list.py
@@ -0,0 +1,11 @@
+from find_max_and_min_of_list import maximum, minimum
+
+
+def test_maximum():
+    given = [1, 2, 3, 4, 5]
+    assert maximum(given) == 5
+
+
+def test_minimum():
+    given = [1, 2, 3, 4, 5]
+    assert minimum(given) == 1
