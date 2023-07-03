cae72a5b9b87763176a9423d1141bf5dec49eaba
patryk_s <boydmalvo@gmail.com>
2023-07-03 20:36:38 +0200

    get_planet_name_by_id

---
+++ codewars/kata_8/get_planet_name_by_id/test_get_planet_name_by_id.py
@@ -0,0 +1,9 @@
+from get_planet_name_by_id import get_planet_name
+import pytest
+
+
+@pytest.mark.parametrize('given, expected',
+                         [(2, 'Venus'), (5, 'Jupiter'), (3, 'Earth'), (4, 'Mars'), (8, 'Neptune'),
+                          (1, 'Mercury')])
+def test_get_planet_name(given, expected):
+    assert get_planet_name(given) == expected
