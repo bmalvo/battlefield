61310b86e0cee49f1cd00325ebb406c2f36be86d
bmalvo <boydmalvo@gmail.com>
2023-08-04 22:15:31 +0200

    how_many_lightsabers_do_you_own

---
+++ codewars/kata_8/how_many_lightsabers_do_you_own/test_how_many_lightsabers_do_you_own.py
@@ -0,0 +1,13 @@
+from how_many_lightsabers_do_you_own import how_many_light_sabers_do_you_own
+
+
+def test_how_many_lightsabers_Zach_arg():
+    assert how_many_light_sabers_do_you_own("Zach") == 18
+
+
+def test_how_many_lightsabers_without_arg():
+    assert how_many_light_sabers_do_you_own() == 0
+
+
+def test_how_many_lightsabers_with_other_arg():
+    assert how_many_light_sabers_do_you_own('Bob') == 0
