d847d8b4e93383e80aa21b35660e9ed9fa685355
bmalvo <boydmalvo@gmail.com>
2023-10-10 13:15:38 +0200

    classic_hello_world

---
+++ codewars/kata_8/classic_hello_world/classic_hello_world.py
@@ -0,0 +1,12 @@
+"""You are given a method called main, make it print the line Hello World!, 
+(yes, that includes a new line character at the end) and don't return anything.
+Note that for some languages, the function main is the entry point 
+of the program."""
+
+
+class Solution:
+
+    @staticmethod
+    def main(*_):
+        print(f'Hello World!')
+    
