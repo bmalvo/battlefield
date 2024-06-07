f12fb79ca9b8a2bb4cc920d458828438428da32b
bmalvo <boydmalvo@gmail.com>
2024-06-07 23:56:29 +0200

    add Wilson_primes

---
+++ codewars/kata_8/wilson_primes/test_wilson_primes.py
@@ -0,0 +1,8 @@
+from wilson_primes import am_i_wilson
+import pytest
+
+
+@pytest.mark.parametrize('given, expected', [(0, False), (3, False), 
+                                             (5, True), (13, True)])
+def test_am_i_wilson(given, expected):
+    assert am_i_wilson(given) == expected
