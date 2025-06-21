09f1dcf5d47996f72a4256311357d3172b95b04f
bmalvo <boydmalvo@gmail.com>
2025-06-21 13:52:57 +0200

    area_of_a_square

--- codewars/kata_8/area_of_a_square/area_of_a_square.py
+++ codewars/kata_8/area_of_a_square/area_of_a_square.py
@@ -1,10 +1,16 @@
-"""Complete the function that calculates the area of the red square, when the 
-length of the circular arc A is given as the input. Return the result rounded 
+"""Complete the function that calculates the area of the red square 
+(file red_square.png in this folder), when the  length of the circular arc A 
+is given as the input. Return the result rounded 
 to two decimals.
 Note: use the π value provided in your language 
 (Math::PI, M_PI, math.pi, etc)"""
 
+from math import pi
 
-def square_area(A):
-    # your code here
-    return
+
+def square_area(circular_arc):
+    """calculate area of a square"""
+
+    r = (4 * circular_arc) / (2 * pi)
+
+    return round(r * r, 2)
--- codewars/kata_8/area_of_a_square/test_area_of_a_square.py
+++ codewars/kata_8/area_of_a_square/test_area_of_a_square.py
@@ -1,1 +1,10 @@
+import pytest
 from area_of_a_square import square_area
+
+@pytest.mark.parametrize('given, expected', [(2, 1.62),
+                                             (0, 0),
+                                             (14.05, 80),
+                                             (1, 0.41), 
+                                             (100, 4052.85)])
+def test_square_area(given, expected):
+    assert square_area(given) == expected
