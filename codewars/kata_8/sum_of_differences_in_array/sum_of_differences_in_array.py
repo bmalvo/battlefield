2cb45138f27be2a5afd5b97ea2db7db15c723795
bmalvo <boydmalvo@gmail.com>
2026-04-13 22:31:20 +0200

    sum_of_differences_in_array

--- codewars/kata_8/sum_of_differences_in_array/sum_of_differences_in_array.py
+++ codewars/kata_8/sum_of_differences_in_array/sum_of_differences_in_array.py
@@ -4,16 +4,12 @@ in descending order."""
 
 def sum_of_differences(arr):
     """output sum of differences"""
-    arr = sorted(arr, reverse=True)
-    sumary = 0
+    arr.sort(reverse=True)
+    summary = 0
     if len(arr) > 1:
-        for i in arr:
-            if arr.index(i) == len(arr) +1:
+        for num in range(len(arr)):
+            if num == (len(arr) -1):
                 break
-            add = i - (arr[arr.index(i) + 1])
-            sumary += abs(add)
-    return  sumary
-
-
-print(sum_of_differences([1, 10, 8, 1, 5, -3, -9, -
-      1, 9, -4, 7, -3, -8, -10, -2, -10, 0, 3, 1]))
+            add = arr[num] - (arr[num +1])
+            summary += add
+    return  summary
--- codewars/kata_8/sum_of_differences_in_array/tempCodeRunnerFile.py
+++ codewars/kata_8/sum_of_differences_in_array/tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-    for i in arr:
-            if i == arr[-1]:
-                print(i)
-                break
-            sum += i - arr.index(i + 1)
-            print(i)
+r[num] - (arr[arr[num] + 1])
+                summary 
